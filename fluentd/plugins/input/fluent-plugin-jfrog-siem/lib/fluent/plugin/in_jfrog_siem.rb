808ca19b0c12d9c07ac1b0423e26e63a36edc69d
MahithaB <60710901+MahithaB@users.noreply.github.com>
2020-09-09 12:06:57 -0700

    Testing string

--- fluentd/plugins/input/fluent-plugin-jfrog-siem/lib/fluent/plugin/in_jfrog_siem.rb
+++ fluentd/plugins/input/fluent-plugin-jfrog-siem/lib/fluent/plugin/in_jfrog_siem.rb
@@ -126,7 +126,7 @@ module Fluent
             # Publish the record to fluentd
             if persistItem
 
-              now = Time.now
+              now = Time.now.strftime('%Y-%m-%dT%H:%M:%S.%LZ')
               router.emit(@tag, now, item)
 
               # write to the pos_file created_date_string
